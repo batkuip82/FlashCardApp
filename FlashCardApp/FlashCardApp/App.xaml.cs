0f233eca5cec34724349715e75d4166b7400ac39
B. Kuipers <bart_k2@hotmail.com>
2017-11-01 22:09:01 +0800

    Updated xamarin & added prism

--- FlashCardApp/FlashCardApp/App.xaml.cs
+++ FlashCardApp/FlashCardApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prism.Unity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,13 +8,13 @@ using Xamarin.Forms;
 
 namespace FlashCardApp
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
-        public App()
+        public App(IPlatformInitializer initializer = null) : base(initializer)
         {
-            InitializeComponent();
+            //InitializeComponent();
 
-            MainPage = new NavigationPage(new FlashCardApp.FlashCardPage());
+            //MainPage = new NavigationPage(new FlashCardApp.FlashCardPage());
         }
 
         protected override void OnStart()
@@ -29,6 +30,16 @@ namespace FlashCardApp
         protected override void OnResume()
         {
             // Handle when your app resumes
+        }
+
+        protected override void OnInitialized()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RegisterTypes()
+        {
+            throw new NotImplementedException();
         }
     }
 }
